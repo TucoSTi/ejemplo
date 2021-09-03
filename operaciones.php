17295ae74864d9c965cb6b5742cc4219a1f568ff
Enzo <you@example.com>
2021-09-03 15:06:17 -0300

    Agrega la division

--- operaciones.php
+++ operaciones.php
@@ -5,6 +5,7 @@ $n2 = $_GET['n2'];
 $suma = $n1 + $n2;
 $resta = $n1 - $n2;
 $multi = $n1 * $n2;
+$divi = $n1 / $n2;
 ?> <!DOCTYPE html>
 <html>
 <head>
@@ -18,5 +19,6 @@ link
 		<?php echo "<p>La suma de $n1 + $n2 = $suma</p>"; ?>
 		<?php echo "<p>El producto entre $n1 * $n2 = $multi</p>"; ?>
 		<?php echo "<p>La resta de $n1 - $n2 = $resta</p>"; ?>
+		<?php echo "<p>La division de $n1 / $n2 = $divi</p>"; ?>
 </body>
 </html>
